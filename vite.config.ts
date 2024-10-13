c2e97c3898da59161dbbdce60a353ba89ab92fec
Francisco Miranda <franciscoabmmiranda@gmail.com>
2024-10-13 22:35:33 +0100

    chore: env setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Above-Challenge',
+  base: '/Above-Challenge/',
   resolve: {
     alias: {
       '@components': '/src/components',
